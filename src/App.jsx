101f2225885e260d0cf1704e5a404ab78f5d89f6
Gabriel Francisco <105892017+GabsFMA@users.noreply.github.com>
2025-03-14 12:46:32 -0300

    feat: set inital configs for app

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className='bg-[#fff] w-[900px] h-[600px] rounded-tl-2xl rounded-tr-2xl rounded-bl-2xl rounded-br-[192px]'>
+      
+    </div>
+  )
+}
+
+export default App
