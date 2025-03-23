0e1f6960a5e260977b62adea3bc18b691640a51a
Gabriel Francisco <105892017+GabsFMA@users.noreply.github.com>
2025-03-22 22:50:24 -0300

    fix: fixing the yo component so the output comes out correctly according to the result of the calculation

--- src/components/yo.tsx
+++ src/components/yo.tsx
@@ -5,17 +5,36 @@ interface YoProps {
     dmy: string;
 }
 
-function Yo({ quantity, dmy }) {
+function Yo({ quantity, dmy }: YoProps) {
+    const getPlural = () => {
+      
+        if (typeof quantity === 'number') {
+            if (dmy === 'meses') return quantity === 1 ? 'mês' : 'meses';
+            return dmy;
+        }
+
+        if (typeof quantity === 'number') {
+          if (dmy === 'dias') return quantity === 1 ? 'dia' : 'dias';
+          return dmy;
+      }
+
+      if (typeof quantity === 'number') {
+        if (dmy === 'anos') return quantity === 1 ? 'ano' : 'anos';
+        return dmy;
+    }
+        return dmy;
+    };
+
     return (
-      <div className="flex flex-row items-end gap-2 lg:gap-4">
-        <span className="text-6xl md:text-7xl lg:text-8xl font-bold text-[#C64CFF]">
-          {quantity}
-        </span>
-        <span className="text-6xl md:text-7xl lg:text-8xl font-bold">
-          {dmy}
-        </span>
-      </div>
+        <div className="flex flex-row items-end gap-2 lg:gap-4">
+            <span className="text-6xl md:text-7xl lg:text-8xl font-bold text-[#C64CFF]">
+                {quantity}
+            </span>
+            <span className="text-6xl md:text-7xl lg:text-8xl font-bold">
+                {getPlural()} 
+            </span>
+        </div>
     );
-  }
+}
 
 export default Yo;
